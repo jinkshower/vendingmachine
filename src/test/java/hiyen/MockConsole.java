79afaf3ae873a86cefbce0862f0ec613bf40c449
jinkshower <askthem@naver.com>
2024-07-03 17:16:09 +0900

    test: MockConsole을 이용한 검증

---
+++ src/test/java/hiyen/MockConsole.java
@@ -0,0 +1,22 @@
+package hiyen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockConsole implements Console {
+    private List<String> inputs = new ArrayList<>();
+
+    @Override
+    public String read() {
+        return null;
+    }
+
+    @Override
+    public void print(final String message) {
+        inputs.add(message);
+    }
+
+    public List<String> getInputs() {
+        return inputs;
+    }
+}
--- src/test/java/hiyen/VendingMachineTest.java
+++ src/test/java/hiyen/VendingMachineTest.java
@@ -7,21 +7,26 @@ import org.junit.jupiter.api.Test;
 
 class VendingMachineTest {
     private VendingMachine vendingMachine;
+    private MockConsole console;
 
     @BeforeEach
     void setup() {
         vendingMachine = new VendingMachine();
+        console = new MockConsole();
     }
 
     @Test
     void 이름으로_음료수를_반환_받을_수_있다() {
         //given
         String wanted = "Coke";
+        String expected = "This is Coke";
 
         //when
         Drink pop = vendingMachine.pop(wanted);
+        String message = pop.drink();
+        console.print(message);
 
         //then
-        pop.drink();
+        assertEquals(expected, console.getInputs().get(0));
     }
 }
